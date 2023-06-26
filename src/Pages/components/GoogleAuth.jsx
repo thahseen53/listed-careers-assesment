7075119e98d13bc4e87ac33ddb943229defa939d
thahseen53 <thahseen53@gmail.com>
2023-06-26 22:31:11 +0530

    oauth

--- src/Pages/components/GoogleAuth.jsx
+++ src/Pages/components/GoogleAuth.jsx
@@ -1,10 +1,15 @@
 import { GoogleLogin } from "@react-oauth/google";
+import { redirect } from "react-router-dom";
 
 const google = () => {
   return (
     <GoogleLogin
       onSuccess={(credentialResponse) => {
-        console.log(credentialResponse);
+        sessionStorage.setItem(
+          "id",
+          JSON.stringify(credentialResponse.idToken)
+        );
+        redirect("/dashboard");
       }}
       onError={() => {
         console.log("Login Failed");
--- src/Pages/components/ProtectedRoute.jsx
+++ src/Pages/components/ProtectedRoute.jsx
@@ -4,8 +4,9 @@ const ProtectedRoute = ({ children }) => {
   const navigate = useNavigate();
   const email = JSON.parse(sessionStorage.getItem("email"));
   const password = JSON.parse(sessionStorage.getItem("password"));
+  const id = JSON.parse(sessionStorage.getItem("id"));
   console.log(email);
-  return email && password ? children : <Navigate to="/" />;
+  return (email && password) || id ? children : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;
